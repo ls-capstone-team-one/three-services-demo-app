573c3e3be3be244014a06b982b5ab1e189f2e74f
Valerie Racine <116108359+v-racine@users.noreply.github.com>
2026-05-04 10:51:21 -0700

    feat(orders): add OpenTelemetry SDK with console exporter (#5)
    * feat(orders): add OpenTelemetry SDK with console exporter
    
    * fix(orders): collapse wrapped template literals in log messages

--- services/orders/src/server.ts
+++ services/orders/src/server.ts
@@ -1,3 +1,6 @@
+import "./infra/telemetry";
+import { shutdownTelemetry } from "./infra/telemetry";
+
 import express from "express";
 import { HttpInventoryClient } from "./infra/httpInventoryClient";
 import { buildRoutes } from "./http/routes";
@@ -40,8 +43,9 @@ const shutdown = (signal: string) => {
     console.error("shutdown timed out, forcing exit");
     process.exit(1);
   }, SHUTDOWN_TIMEOUT_MS);
-  server.close(() => {
+  server.close(async () => {
     clearTimeout(timer);
+    await shutdownTelemetry();
     process.exit(0);
   });
 };
